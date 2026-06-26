e78f01b10bd2cc72cbcaeeeb082eceba3122e7b0
Aeonfall <rewbe563@gmail.com>
2026-06-26 18:03:36 -0500

    Create tailwind-config.js

---
+++ tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'parchment': '#f1e5c5',
+                'ink': '#2c1e16',
+                'blood': '#7a1b1b',
+                'gold': '#c5a059',
+                'wood': '#1a110b',
+                'stone': '#e8e0cd'
+            },
+            fontFamily: {
+                'serif': ['Lora', 'serif'],
+                'heading': ['Cinzel', 'serif'],
+            }
+        }
+    }
+}
